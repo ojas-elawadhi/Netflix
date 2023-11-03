2f70c980871d708911589552591bcdffdba44671
Ojas Elawadhi <ojaselawadhi1@gmail.com>
2023-11-04 01:57:54 +0530

    mobile height scroll

--- src/components/Login.js
+++ src/components/Login.js
@@ -95,7 +95,7 @@ const Login = () => {
       <Header />
       <div className="absolute -z-1 block bg-cover overflow-hidden overflow-clip-margin h-[100dvh] w-full">
         <img
-          className="concord-img vlv-creative min-h-[100%] min-w-[1800px] w-[100vw] xl:w-full"
+          className="concord-img vlv-creative min-h-[100dvh] min-w-[1800px] w-[100vw] xl:w-full"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/ab4b0b22-2ddf-4d48-ae88-c201ae0267e2/0efe6360-4f6d-4b10-beb6-81e0762cfe81/IN-en-20231030-popsignuptwoweeks-perspective_alpha_website_small.jpg"
           srcSet="https://assets.nflxext.com/ffe/siteui/vlv3/ab4b0b22-2ddf-4d48-ae88-c201ae0267e2/0efe6360-4f6d-4b10-beb6-81e0762cfe81/IN-en-20231030-popsignuptwoweeks-perspective_alpha_website_small.jpg 1000w, https://assets.nflxext.com/ffe/siteui/vlv3/ab4b0b22-2ddf-4d48-ae88-c201ae0267e2/0efe6360-4f6d-4b10-beb6-81e0762cfe81/IN-en-20231030-popsignuptwoweeks-perspective_alpha_website_medium.jpg 1500w, https://assets.nflxext.com/ffe/siteui/vlv3/ab4b0b22-2ddf-4d48-ae88-c201ae0267e2/0efe6360-4f6d-4b10-beb6-81e0762cfe81/IN-en-20231030-popsignuptwoweeks-perspective_alpha_website_large.jpg 1800w"
           alt=""
